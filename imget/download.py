3cd7aadc1bf3818be3d5eb08861059096b019a07
Tiger Nie <nhl0819@gmail.com>
2020-05-29 14:54:46 -0500

    refactored parser, changed output file names to numeric values instead of url basename

--- imget/download.py
+++ imget/download.py
@@ -18,24 +18,35 @@ def _download_url(url: str, out_path: str):
         print(log_)
 
 
+_counter = 0
+
+
+def _get_filename(out_dir: str, basename: str):
+    global _counter
+    _counter += 1
+    _, ext = os.path.splitext(basename)
+    out_path = os.path.join(out_dir, str(_counter) + ext)
+    return out_path
+
+
 def download_url_list(urls: List[str], out_dir: str, max_workers=None):
     """
     Download url list to target directory concurrently
     using python threads
     """
-    # Download all image img_links to new directory
+    # Download all image urls to new directory
     print(f"Downloading {len(urls)} files . . .")
 
     with Executor(max_workers=max_workers) as exe:
         jobs = []
         for url in urls:
             basename = url.split("/")[-1]
-            out_path = os.path.join(out_dir, basename)
+            out_path = _get_filename(out_dir, basename)
 
             # Download and write image
             jobs.append(exe.submit(_download_url, url, out_path))
 
-        results = [job.result() for job in jobs]
+        # results = [job.result() for job in jobs]
 
     print(f"Downloaded {len(urls)} files.")
 
--- imget/parser.py
+++ imget/parser.py
@@ -9,7 +9,20 @@ from bs4 import BeautifulSoup  # HTML Parser
 SUFFIXES = ("jpg", "jpeg", "png", "gif")
 
 
-def parse_url_target(url: str, class_=None, id_=None) -> Tuple[str, List[str]]:
+def _remove_query_string(url: str) -> str:
+    """
+    Remove query string from url
+
+    """
+    parse_res = urlparse(url)
+    parse_res = parse_res._replace(query="")
+    url = parse_res.geturl()
+    return url
+
+
+def parse_url_target(
+    url: str, class_=None, id_=None, tags=["a", "img"]
+) -> Tuple[str, List[str]]:
     """
     Arguments:
         url - (str) URL of the page to parse. This function
@@ -27,10 +40,7 @@ def parse_url_target(url: str, class_=None, id_=None) -> Tuple[str, List[str]]:
     if not url.startswith("http"):
         url = "http://" + url
 
-    parse_res = urlparse(url)
-    # Remove query string
-    parse_res = parse_res._replace(query="")
-    url = parse_res.geturl()
+    url = _remove_query_string(url)
 
     # Get initial url
     resp = requests.get(url)
@@ -39,7 +49,9 @@ def parse_url_target(url: str, class_=None, id_=None) -> Tuple[str, List[str]]:
     soup = BeautifulSoup(resp.text, "lxml")
 
     # Get title for new dir name
-    title = soup.title.string
+    # HTML pge title is often a bad name for a directory, so
+    # we'll use the last section of the url
+    title = url.split("/")[-1]
 
     # If after filtering by class_, soup_ is not empty
     # set soup to soup_
@@ -59,27 +71,59 @@ def parse_url_target(url: str, class_=None, id_=None) -> Tuple[str, List[str]]:
         else:
             print(f"Warning: id '{id_}' did not match any elements, ignoring.")
 
-    # Get all a tags with img tags inside, and their links
-    a_tags = soup.select("a[href]")
-    img_links = [
-        t.get("href")
-        for t in a_tags
-        if t.findChild("img") and t.get("href").endswith(SUFFIXES)
-    ]
-    for a_tag in a_tags:
-        img_tag = a_tag.findChild("img")
-        if img_tag:
-            if a_tag.get("href").endswith(SUFFIXES):
-                img_links.append(a_tag.get("href"))
-            else:
-                img_links.append(img_tag.get("src"))
+    img_links = []
+    for _tag in tags:
+        #
+        # Check all 'a' tags that contain an 'img' tag within. Sometimes the
+        # highest resolution images are linked by the 'a' tag.
+        #
+        if _tag == "a":
+            # Get all a tags with img tags inside, and their links
+            a_tags = soup.select("a[href]")
+            img_links.extend(
+                [
+                    t.get("href")
+                    for t in a_tags
+                    if t.findChild("img") and t.get("href").endswith(SUFFIXES)
+                ]
+            )
+            for a_tag in a_tags:
+                img_tag = a_tag.findChild("img")
+                if img_tag:
+                    if a_tag.get("href").endswith(SUFFIXES):
+                        img_links.append(a_tag.get("href"))
+                    elif img_tag.get("src"):
+                        img_links.append(img_tag.get("src"))
+        #
+        # Check 'img' tags themselves. First check the 'data-srcset' and 'srcset',
+        # parsing them to get the highest resolution available. If neither are
+        # available, fall back to 'src'
+        #
+        elif _tag == "img":
+            img_tags = soup.select("img")
+            for img_tag in img_tags:
+                good_link = None
+
+                if img_tag.get("data-srcset"):
+                    _links = img_tag.get("data-srcset")
+                    good_link = _links.split(",")[-1].strip().split(" ")[0]
+
+                if not good_link and img_tag.get("srcset"):
+                    _links = img_tag.get("srcset")
+                    good_link = _links.split(",")[-1].strip().split(" ")[0]
+
+                if not good_link:
+                    good_link = img_tag.get("src")
+
+                if good_link:
+                    img_links.append(good_link)
 
     if not img_links:
         print("Warning: No image links were found.")
         sys.exit(1)
 
     # Simple validation
-    img_links = [l for l in img_links if l.startswith("http")]
+    img_links = [_remove_query_string(l) for l in img_links if l.startswith("http")]
 
     return title, img_links
 
