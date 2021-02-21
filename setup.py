ffb15254d5ade641448c7059db5baf76d6f04e39
Tiger Nie <nhl0819@gmail.com>
2021-02-21 17:04:25 +0800

    removed old download code, added aio deps

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name="imget",
-    version="0.0.1",
+    version="0.1.0",
     description="CLI that downloads images from the main content of a target URL",
     author="Tiger Nie",
     packages=["imget"],
     scripts=["bin/imget"],
-    install_requires=["tqdm", "bs4", "lxml"],
+    install_requires=["tqdm", "bs4", "lxml", "requests", "aiofiles", "aiohttp"],
 )
