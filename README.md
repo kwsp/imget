084f83981caec15d288506432f81f7e9cee45077
Tiger Nie <nhl0819@gmail.com>
2021-02-21 17:35:56 +0800

    license, version

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # imget
 
-Simple image scraper.
+A command line program and library for quickly downloads images from the internet. Internally, all file downloads happen concurrently using python3's asyncio library.
--- setup.py
+++ setup.py
@@ -1,11 +1,30 @@
 from distutils.core import setup
 
+# Get version without importing
+exec(compile(open("imget/version.py").read(), "imget/version.py", "exec"))
+
 setup(
     name="imget",
-    version="0.1.0",
-    description="CLI that downloads images from the main content of a target URL",
+    version=__version__,
+    description="Command-line program and library for parsing and downloading images from the web.",
     author="Tiger Nie",
+    author_email="nhl0819@gmail.com",
+    url="https://github.com/NieTiger/imget",
+    license="mit",
     packages=["imget"],
+    python_requires=">=3.7",
     scripts=["bin/imget"],
     install_requires=["tqdm", "bs4", "lxml", "requests", "aiofiles", "aiohttp"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Multimedia :: Graphics",
+    ],
 )
